50ea59c919a08f1d726be88ace65225a02e65aa1
jitornero <84686534+jitornero@users.noreply.github.com>
2024-01-02 15:19:24 -0300

    Add files via upload

---
+++ prueba github sync-1.0.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         prueba github sync
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.google.com.ar
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/jitornero/01.js/main/01.js?token=GHSAT0AAAAAACLTV4QMNPZMY6JQVG5KUTUCZMUJL7Q
+// @downloadURL  https://raw.githubusercontent.com/jitornero/01.js/main/01.js?token=GHSAT0AAAAAACLTV4QMNPZMY6JQVG5KUTUCZMUJL7Q
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+alert('hola');
+})();
